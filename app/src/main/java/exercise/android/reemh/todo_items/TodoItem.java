8a2b57a14c25a0dd3a0ed848aa89c528f8df92ec
Daniel Abayev <danieldaniel151@gmail.com>
2021-05-14 19:58:47 +0300

    basic TodoItem impl

--- app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
+++ app/src/main/java/exercise/android/reemh/todo_items/TodoItem.java
@@ -3,5 +3,18 @@ package exercise.android.reemh.todo_items;
 import java.io.Serializable;
 
 public class TodoItem implements Serializable {
-  // TODO: edit this class as you want
+    // TODO: edit this class as you want
+    private String description;
+    private String status;
+
+    public TodoItem(String description, String status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public void changeStatus(String newStatus) {
+        this.status = newStatus;
+    }
+
+
 }
